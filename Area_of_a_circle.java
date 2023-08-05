40df56e402b8ad644a97069aa779d0d9a2d13089
SowjanyaLingolu <119915201+SowjanyaLingolu@users.noreply.github.com>
2023-08-05 11:36:23 +0530

    import java.util.Scanner; public class areac{     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int R=sc.nextInt();         double C=3.14*R*R;         System.out.printf("%.2f",C);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class areac{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int R=sc.nextInt();
+        double C=3.14*R*R;
+        System.out.printf("%.2f",C);
+    }
+}
