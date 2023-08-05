e1c80d52b88c5f68eff7b00ae84c4513a90741e4
SowjanyaLingolu <119915201+SowjanyaLingolu@users.noreply.github.com>
2023-08-05 11:22:10 +0530

    import java.util.Scanner; public class type{     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String S=sc.nextLine();         System.out.println(S);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class type{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String S=sc.nextLine();
+        System.out.println(S);
+    }
+}
