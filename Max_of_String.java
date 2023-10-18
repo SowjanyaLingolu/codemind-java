c519b99b22c4594deec86beb1ac398974e3dc19e
SowjanyaLingolu <119915201+SowjanyaLingolu@users.noreply.github.com>
2023-10-18 16:11:42 +0530

    import java.util.*; public class max{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         String s = sc.nextLine();         int sum = 0;         for(int i=0;i<s.length();i++){             char ch=s.charAt(i);             if(ch>sum)             {                 sum=ch;             }         }         System.out.print((char)sum);     } }

---
+++ Max_of_String.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class max{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int sum = 0;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch>sum)
+            {
+                sum=ch;
+            }
+        }
+        System.out.print((char)sum);
+    }
+}
